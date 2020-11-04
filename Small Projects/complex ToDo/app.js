36e318eab2c146fc68e4076ab9359757260e94b3
pantchayan <pantchayan@gmail.com>
2020-11-04 22:41:56 +0530

    ToDo complete

--- Small Projects/complex ToDo/app.js
+++ Small Projects/complex ToDo/app.js
@@ -1,20 +1,27 @@
-let addForm = document.querySelector("body > div > form")
 
+let addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const findForm = document.querySelector(".search input");
+
+
+
+
+// ADDING NEW TASKS ==========================================================
 addForm.addEventListener("submit",(e)=>{
     e.preventDefault();
     addTask(e.path[0][0].value);
     e.path[0][0].value ="";
 });
 
-const ul = document.querySelector("ul");
-
 const addTask = (task)=>{
-    ul.innerHTML+= `<li class="list-group-item d-flex justify-content-between align-items-center">
+    list.innerHTML+= `<li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${task}</span>
     <i class="far fa-trash-alt delete"></i>
   </li>`
 };
 
+// REMOVING TASKS ============================================================
 
 const delBtn = document.querySelector("ul");
 
@@ -23,5 +30,35 @@ delBtn.addEventListener("click",e=>{
         e.path[1].remove();
     } 
 });
+
+
+// FINDING/ FILTERING TASKS ==============================================================
+
+findForm.addEventListener("keyup",e=>{
+    filterToDos(e.path[0].value);
+})
+
+const filterToDos = (term) => {
+    // console.log(Array.from(list.children));
+    console.log(term);
+    Array.from(list.children)
+        .filter((item)=>{
+            return !item.textContent.includes(term);
+        })
+        .forEach((item)=>{
+            item.classList.add('filtered');
+        })
+
+    Array.from(list.children)
+        .filter((item)=>{
+            return item.textContent.includes(term);
+        })
+        .forEach((item)=>{
+            item.classList.remove('filtered');
+        })
+};
+
+
+
 
 
