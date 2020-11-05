05f2303375b9ea06d4b62210c5955cc55ec5dd0f
pantchayan <pantchayan@gmail.com>
2020-11-05 13:56:57 +0530

    learning dates and time

---
+++ Small Projects/days difference calculator/script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+// const now = new Date();
+
+// console.log(now);
+
+// console.log(typeof now);
+
+
+// // Getting year, month, date, day, time ==========================================
+
+
+// console.log(now.getDate());
+// console.log(now.getMonth());
+// console.log(now.getFullYear());
+// console.log(now.getDay());
+
+
+// // Time stamp ====================================================================
+
+// // number of milliseconds since 1st Jan 1970. 
+// console.log(now.getTime());
+
+// // Date strings ==================================================================
+
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleDateString());
+
+
