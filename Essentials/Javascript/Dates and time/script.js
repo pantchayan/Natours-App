94ca203ccaa7dfee5fa9e429aae0684eaab48cc3
pantchayan <pantchayan@gmail.com>
2020-11-05 14:13:28 +0530

    implemented days difference calculator

--- Essentials/Javascript/Dates and time/script.js
+++ Essentials/Javascript/Dates and time/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const now = new Date();
 
 console.log(now);
--- Small Projects/days difference calculator/script.js
+++ Small Projects/days difference calculator/script.js
@@ -1,33 +1,27 @@
+const past = document.querySelector(".date");
+const now = new Date();
+const h2 = document.querySelector(".display-diff");
+
+// fetching date from the form
+
+past.addEventListener("submit",e=>{
+    e.preventDefault();
+    let pastDateStr = e.path[0][0].value; 
+    calculateDiff(pastDateStr);
+});
 
 
+// calculating difference from the input date
 
+const calculateDiff = (pastDateStr) =>{
+    let pastDate = new Date(pastDateStr);
+    let timestampDiff = now.getTime() - pastDate.getTime();
+    let daysDiff = Math.round(timestampDiff/1000/60/60/24);
+    displayDate(daysDiff);
+};
 
+// displaying difference in dates 
 
-// const now = new Date();
-
-// console.log(now);
-
-// console.log(typeof now);
-
-
-// // Getting year, month, date, day, time ==========================================
-
-
-// console.log(now.getDate());
-// console.log(now.getMonth());
-// console.log(now.getFullYear());
-// console.log(now.getDay());
-
-
-// // Time stamp ====================================================================
-
-// // number of milliseconds since 1st Jan 1970. 
-// console.log(now.getTime());
-
-// // Date strings ==================================================================
-
-// console.log(now.toDateString());
-// console.log(now.toTimeString());
-// console.log(now.toLocaleDateString());
-
-
+const displayDate = (daysDiff)=>{
+    h2.textContent = "Its been "+ daysDiff+" days since the date.";
+};
