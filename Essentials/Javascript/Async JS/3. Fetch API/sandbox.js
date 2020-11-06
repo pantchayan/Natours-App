69cd8dcb626d8a035e5245d7c9d9069c5c41d9fe
pantchayan <pantchayan@gmail.com>
2020-11-06 14:10:31 +0530

    the fetch API

---
+++ Essentials/Javascript/Async JS/3. Fetch API/sandbox.js
@@ -0,0 +1,36 @@
+// FETCH API WORKS ON THE METHODOLOGY OF PROMISES
+
+
+
+// Chaining fetch APIs.
+
+fetch("ToDos/Chayan.json").then(resolved=>{
+    if(resolved.ok){
+        return resolved.json(); // CHAINED PROMISE TO RESOLVE THE JSON FILE
+    }
+    else{
+        console.log("Some error.")
+    }
+}).then(data=>{
+    console.log(data);
+    return fetch("ToDos/Tanshi.json");
+}).then(resolved=>{
+    if(resolved.ok){
+        return resolved.json(); // CHAINED PROMISE TO RESOLVE THE JSON FILE
+    }
+    else{
+        console.log("Some error.")
+    }
+}).then(data=>{
+    console.log(data);
+    return fetch("ToDos/Vansika.json");
+}).then(resolved=>{
+    if(resolved.ok){
+        return resolved.json(); // CHAINED PROMISE TO RESOLVE THE JSON FILE
+    }
+    else{
+        console.log("Some error.")
+    }
+}).then(data=>{
+    console.log(data);
+});
