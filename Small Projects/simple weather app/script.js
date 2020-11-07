e13b00f16282c8b7e370ac6a98a31624fdb0db9b
pantchayan <pantchayan@gmail.com>
2020-11-07 21:11:13 +0530

    hitting accuweather API

---
+++ Small Projects/simple weather app/script.js
@@ -0,0 +1,31 @@
+// http://dataservice.accuweather.com/locations/v1/search?q=kolkata&apikey=BvN8yz7JTkcgWAKo8ebI4eqrEugnRMKv
+
+
+const input = document.querySelector(".location-input");
+
+
+
+// getting the input location string ==================================
+
+input.addEventListener("submit",e=>{
+    e.preventDefault();
+    let location = e.target[0].value.toLowerCase();
+    getData(location);
+})
+
+
+// getData method to hit api and recieve JSON data ======================
+
+
+const getData = (location)=>{
+    let URL = "http://dataservice.accuweather.com/locations/v1/search?q="+location+"&apikey=BvN8yz7JTkcgWAKo8ebI4eqrEugnRMKv";
+    // fetch behaves as a promise
+    fetch(URL)
+    .then(response=>response.json())
+    .then(data=>{
+        // process the data.
+        console.log(data);
+    }).catch(err=>{
+        console.log(err);
+    });
+}
