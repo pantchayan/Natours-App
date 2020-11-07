5fcef4a804280cfa013f8abc714ec20163bf8bef
pantchayan <pantchayan@gmail.com>
2020-11-07 22:21:14 +0530

    hitting current condition API

--- Small Projects/simple weather app/script.js
+++ Small Projects/simple weather app/script.js
@@ -3,6 +3,7 @@
 
 const input = document.querySelector(".location-input");
 
+const inputText = document.querySelector("form>input");
 
 
 // getting the input location string ==================================
@@ -14,7 +15,7 @@ input.addEventListener("submit",e=>{
 })
 
 
-// getData method to hit api and recieve JSON data ======================
+// getLocationKey method to hit api and recieve JSON data which includes KEY FOR THE PROVDED LOCATION ======================
 
 
 const getLocationKey = (location)=>{
@@ -23,9 +24,38 @@ const getLocationKey = (location)=>{
     fetch(URL)
     .then(response=>response.json())
     .then(data=>{
-        // process the data.
-        console.log(data);
+        // process the data and get location key.
+
+        if(data.length >0){
+        console.log(data[0].Key);
+        let locationKey = data[0].Key;
+        getData(locationKey);
+        }
+        else{
+            alert("Please enter a valid place.");
+            inputText.value="";
+        }
     }).catch(err=>{
         console.log(err);
     });
 }
+
+
+// Using location key to hit Current Conditions API .
+
+//http://dataservice.accuweather.com/currentconditions/v1/206690?apikey=BvN8yz7JTkcgWAKo8ebI4eqrEugnRMKv&language=en-us&details=false
+
+
+const getData = (Key) =>{
+    let URL = "http://dataservice.accuweather.com/currentconditions/v1/"+Key+"?apikey=BvN8yz7JTkcgWAKo8ebI4eqrEugnRMKv&language=en-us&details=false";
+
+    fetch(URL)
+    .then(response=>response.json())
+    .then(data=>{
+        // Process data 
+        console.log(data[0].Temperature.Metric.Value,data[0].Temperature.Metric.Unit);
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+}   
