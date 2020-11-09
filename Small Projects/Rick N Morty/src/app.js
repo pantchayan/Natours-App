18f85ae00cf6e50cf33f76164b372609155a319a
pantchayan <pantchayan@gmail.com>
2020-11-10 01:09:28 +0530

    rick n morty completed

--- Small Projects/Rick N Morty/src/app.js
+++ Small Projects/Rick N Morty/src/app.js
@@ -3,6 +3,9 @@
 // CONST ==> constant , LET ==> update
 
 const input = document.querySelector(".char-input form");
+const resultText = document.querySelector("#result > div > p");
+const resultImage = document.querySelector("#result > div > img");
+
 
 
 input.addEventListener("click" , e=>{
@@ -25,11 +28,26 @@ const getData = (characterId) =>{
     .then(response=>response.json())
     .then(data=>{
         let charData = data;
-        console.log(charData);
-        
+        showData(charData);
     })
     .catch(err=>{
         console.log(err);
     });
     
+};
+
+const showData = (charData) =>{
+    console.log(charData);
+
+    // console.log(resultImage);
+
+    console.log(resultText.innerHTML);
+    resultText.innerHTML = `<b>Name:</b> ${charData.name}<br>
+    <b>Status:</b> ${charData.status}<br>
+    <b>Species:</b> ${charData.species}<br>
+    <b>Gender:</b> ${charData.gender}<br>
+    <b>Location:</b> ${charData.location.name}<br>
+    <b>Origin:</b> ${charData.origin.name}`;
+    resultImage.src = charData.image;
+    window.scrollTo(0,document.body.scrollHeight);
 };
