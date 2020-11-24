46fb9bf2f36bb089f6e0d91a07182e85ff1dd442
pantchayan <pantchayan@gmail.com>
2020-11-24 12:09:35 +0530

    updating time without DateFNS

--- Small Projects/Chatroom WebApp/scripts/ui.js
+++ Small Projects/Chatroom WebApp/scripts/ui.js
@@ -1,7 +1,6 @@
 // render chat templates to the DOM
 // clear the list of chats  (when the room changes)
 
-
 class chatUI{
     constructor(list){
         this.list = list;
@@ -11,10 +10,9 @@ class chatUI{
     }
     render(data){
         //data ==> chat object
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            { addSuffix:true }
-          );
+        const prev = data.created_at.toDate().getTime();
+        const curr = new Date().getTime();
+        const when = timeDifference(curr,prev);
         const html = `
             <li class="list-group-item">
                 <span class="username">${data.username}</span>
@@ -25,5 +23,40 @@ class chatUI{
 
         console.log("I am inside render");
         this.list.innerHTML += html;
+    }
+}
+
+function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return  Math.round(elapsed/msPerYear ) + ' years ago';   
     }
 }
