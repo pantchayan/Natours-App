fae0f40bd1947757ac468323e01f075c72cb8765
pantchayan <pantchayan@gmail.com>
2020-12-02 21:40:04 +0530

    naive server using Node

--- Back-end/NodeJS/1-node-farm/index.js
+++ Back-end/NodeJS/1-node-farm/index.js
@@ -1,4 +1,30 @@
 const fs = require('fs');
+const http = require('http');
+
+let data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+let dataObj = JSON.parse(data);
 
 
-console.log(fs);
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is an overview");
+    }
+    else if (pathName === "/product") {
+        res.end("This is a product");
+    }
+    else if (pathName === "/api") {
+
+        res.writeHead(200, { "Content_type": "application/json" });
+        res.end(data);
+    }
+    else {
+        res.writeHead(404);
+        res.end("Page not found");
+    }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening to requests on port 8000");
+});
