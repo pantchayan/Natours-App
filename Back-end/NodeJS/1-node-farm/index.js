4ba210b419aef70dc9992af581400afbbb47447b
pantchayan <pantchayan@gmail.com>
2020-12-03 12:18:02 +0530

    importing own modules

--- Back-end/NodeJS/1-node-farm/index.js
+++ Back-end/NodeJS/1-node-farm/index.js
@@ -1,21 +1,12 @@
+// CORE MODULES
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-const replaceTemplate = (temp, product)=>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-    output = output.replace(/{%PRICE%}/g,product.price);
-    output = output.replace(/{%IMAGE%}/g,product.image);
-    output = output.replace(/{%QUANTITY%}/g,product.quantity);
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-    output = output.replace(/{%DESCRIPTION%}/g,product.description);
-    output = output.replace(/{%ID%}/g,product.id);
-    output = output.replace(/{%FROM%}/g,product.from);
+// OWN MODULES
+const replaceTemplate = require('./modules/replaceTemplate');
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/,"not-organic");
 
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
