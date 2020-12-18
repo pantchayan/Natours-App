f223f7b1b1070c4602857fe303b949a3aad80558
pantchayan <pantchayan@gmail.com>
2020-12-18 23:13:30 +0530

    18th Sept

--- Back-end/NodeJS/1-node-farm/index.js
+++ Back-end/NodeJS/1-node-farm/index.js
@@ -8,9 +8,9 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+    const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+    const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+    const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
